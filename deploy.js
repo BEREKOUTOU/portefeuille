421f79311af6a9f692afd7e8869a151cd83662f2
berekoutou boniface <allahtoralphdjamel@gmail.com>
2025-05-26 17:29:25 +0200

    Refactor deployment script to use dynamic import for gh-pages and update package dependencies

--- deploy.js
+++ deploy.js
@@ -1,14 +1,20 @@
-import ghpages from 'gh-pages';
+(async () => {
+  const ghpages = await import('gh-pages');
 
-ghpages.publish('dist', {
-  branch: 'gh-pages',
-  repo: 'https://github.com/BEREKOUTOU/portefeuille.git',
-  dotfiles: true
-}, (err) => {
-  if (err) {
-    console.error('Deploy failed:', err);
-    process.exit(1);
-  } else {
-    console.log('Deploy successful!');
-  }
-});
+  console.log('ghpages:', ghpages);
+  const options = {
+    branch: 'gh-pages',
+    repo: 'https://github.com/BEREKOUTOU/portefeuille.git',
+    dotfiles: true
+  };
+  console.log('options:', options);
+
+  ghpages.default.publish('dist', options, (err) => {
+    if (err) {
+      console.error('Deploy failed:', err);
+      process.exit(1);
+    } else {
+      console.log('Deploy successful!');
+    }
+  });
+})();
