4e945a80e4d8fde7f60379c572b5461473dc8c6d
berekoutou boniface <allahtoralphdjamel@gmail.com>
2025-08-07 21:57:45 +0200

    `Update subproject commit and add React import in src/main.tsx`

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -6,7 +7,9 @@ import './i18n';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
   <BrowserRouter basename="/portefeuille">
     <App />
   </BrowserRouter>
+  </React.StrictMode>
 );
